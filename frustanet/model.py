eb8b63f70811167c18c4fc900264845698352689
cbbruss <cbbruss@gmail.com>
2021-01-10 18:42:54 -0500

    Add more horizontally scaling layers

--- frustanet/model.py
+++ frustanet/model.py
@@ -17,8 +17,24 @@ class FrustaNetRegression(LightningModule):
         """
         self.linear = torch.nn.Linear(n_features, 1)
 
-        # self.layer_2 = torch.nn.Linear(128, 256)
-        # self.layer_3 = torch.nn.Linear(256, 10)
+        self.f2_layer0 = torch.nn.Linear(n_features, n_features*2)
+        self.f2_bnorm = torch.nn.BatchNorm1d(n_features*2)
+        self.f2_layer1 = torch.nn.Linear(n_features*2, 1)
+
+        self.f3_layer0 = torch.nn.Linear(n_features, n_features*4)
+        self.f3_bnorm0 = torch.nn.BatchNorm1d(n_features*4)
+        self.f3_layer1 = torch.nn.Linear(n_features*4, n_features*2)
+        self.f3_bnorm1 = torch.nn.BatchNorm1d(n_features*2)
+        self.f3_layer2 = torch.nn.Linear(n_features*2, 1)
+
+        self.f4_layer0 = torch.nn.Linear(n_features, n_features*8)
+        self.f4_bnorm0 = torch.nn.BatchNorm1d(n_features*8)
+        self.f4_layer1 = torch.nn.Linear(n_features*8, n_features*4)
+        self.f4_bnorm1 = torch.nn.BatchNorm1d(n_features*4)
+        self.f4_layer2 = torch.nn.Linear(n_features*4, n_features*2)
+        self.f4_bnorm2 = torch.nn.BatchNorm1d(n_features*2)
+        self.f4_layer3 = torch.nn.Linear(n_features*2, 1)
+
 
     def forward(self, x):
         """
@@ -30,15 +46,63 @@ class FrustaNetRegression(LightningModule):
             Returns:
                 out: model prediction
         """
-        out = self.linear(x)
-        return out
+
+        out_linear = self.linear(x)
+
+        out_f2 = self.f2_layer0(x)
+        out_f2 = self.f2_bnorm(out_f2)
+        out_f2 = torch.tanh(out_f2)
+        out_f2 = self.f2_layer1(out_f2)
+
+        out_f3 = self.f3_layer0(x)
+        out_f3 = self.f3_bnorm0(out_f3)
+        out_f3 = torch.tanh(out_f3)
+        out_f3 = self.f3_layer1(out_f3)
+        out_f3 = self.f3_bnorm1(out_f3)
+        out_f3 = torch.tanh(out_f3)
+        out_f3 = self.f3_layer2(out_f3)
+
+        out_f4 = self.f4_layer0(x)
+        out_f4 = self.f4_bnorm0(out_f4)
+        out_f4 = torch.tanh(out_f4)
+        out_f4 = self.f4_layer1(out_f4)
+        out_f4 = self.f4_bnorm1(out_f4)
+        out_f4 = torch.tanh(out_f4)
+        out_f4 = self.f4_layer2(out_f4)
+        out_f4 = self.f4_bnorm2(out_f4)
+        out_f4 = torch.tanh(out_f4)
+        out_f4 = self.f4_layer3(out_f4)
+
+        out_final = out_linear + out_f2 + out_f3 + out_f4
+        # out_final = out_linear
+
+
+        return out_final, out_linear, out_f2, out_f3, out_f4
 
     def training_step(self, batch, batch_idx):
         mseloss = nn.MSELoss()
         x, y = batch
-        y_hat = self.forward(x)
+        y_hat, y_linear, y_f2, y_f3, y_f4 = self(x)
+        l_linear = mseloss(y_linear, y)
+        l_f2 = mseloss(y_f2, y)
+        l_f3 = mseloss(y_f3, y)
+        l_f4 = mseloss(y_f4, y)
         loss = mseloss(y_hat, y)
+
+        self.log('Loss', loss, on_step=True)
+        self.log('Linear Loss', l_linear, on_step=True)
+        self.log('F2 Loss', l_f2, on_step=True)
+        self.log('F3 Loss', l_f3, on_step=True)
+        self.log('F4 Loss', l_f4, on_step=True)
         return loss
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=1e-3)
+        return Adam(self.parameters(), lr=0.001, weight_decay=0.1)
+
+    def validation_step(self, batch, batch_idx):
+        mseloss = nn.MSELoss()
+        x, y = batch
+        y_hat, y_linear, y_f2, y_f3, y_f4 = self(x)
+        loss = mseloss(y_hat, y)
+        self.log('Val Loss', loss)
+
--- regression_example.py
+++ regression_example.py
@@ -2,9 +2,12 @@ import os
 import torch
 from torch import nn, tensor
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from torch.utils.data import DataLoader, TensorDataset
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_boston, fetch_california_housing
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+import numpy as np
 
 
 from frustanet.model import FrustaNetRegression
@@ -12,21 +15,48 @@ from frustanet.model import FrustaNetRegression
 
 def main():
 
-    X, y = load_boston(return_X_y=True)
+    X, y = fetch_california_housing(return_X_y=True)
     n_features = X.shape[1]
 
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test_valid, y_train, y_test_valid = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_test, X_valid, y_test, y_valid = train_test_split(X_test_valid, y_test_valid, test_size=0.5, random_state=42)
 
-    X_train = tensor(X, dtype=torch.float)
-    y_train = tensor(y, dtype=torch.float)
+    train_mean = X_train.mean(axis=0)
+    train_std = X_train.std(axis=0)
+
+    X_train = (X_train - train_mean) / train_std
+    X_valid = (X_valid - train_mean) / train_std
+    X_test = (X_test - train_mean) / train_std
+    
 
 
-    datasets = TensorDataset(X_train, y_train)
 
-    train_iter = DataLoader(datasets, batch_size=10, shuffle=True)
+    X_train = tensor(X_train, dtype=torch.float)
+    X_valid = tensor(X_valid, dtype=torch.float)
+    X_test = tensor(X_test, dtype=torch.float)
+    y_train = tensor(y_train, dtype=torch.float).reshape(-1, 1)
+    y_valid = tensor(y_valid, dtype=torch.float).reshape(-1, 1)
+    y_test = tensor(y_test, dtype=torch.float).reshape(-1, 1)
+
+    train_datasets = TensorDataset(X_train, y_train)
+    valid_datasets = TensorDataset(X_valid, y_valid)
+
+    train_loader = DataLoader(train_datasets, batch_size=256, shuffle=True)
+    valid_loader = DataLoader(valid_datasets)
     f_model = FrustaNetRegression(n_features=n_features)
-    trainer = Trainer()
-    trainer.fit(f_model, train_iter)
+    trainer = Trainer(callbacks=[EarlyStopping(monitor='Val Loss')])
+    trainer.fit(f_model, train_loader, valid_loader)
+    f_preds = f_model.forward(X_valid)
+    print(np.mean((f_preds[0].flatten().detach().numpy() - y_valid.flatten().numpy()) ** 2))
+
+    ## Training Comparison GBM
+    reg = GradientBoostingRegressor(random_state=0, verbose=True)
+
+    reg.fit(X_train, y_train)
+
+    preds = reg.predict(X_valid)
+
+    print(np.mean((preds - y_valid.flatten().numpy()) ** 2))
 
 if __name__ == '__main__':
     main()
