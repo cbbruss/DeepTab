2796f97cf798994314790a5cd0c15257a295b521
Bayan Bruss <cbbruss@gmail.com>
2021-01-28 05:00:19 -0500

    Rename files and update readme (#2)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="frustanet", # Replace with your own username
+    name="deeptab", # Replace with your own username
     version="0.0.1",
     author="Bayan Bruss",
     author_email="cbbruss@example.com",
     description="A Neural Network for Tabular Data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cbbruss/frustanet",
+    url="https://github.com/cbbruss/deeptab",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
